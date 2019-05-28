738e01c5b9cf9af840ae84b8a26e14cff93e1fae
larssonmartin1998 <contact@erikmartinlarsson.com>
2019-05-28 17:06:08 +0200

    Organize Build.cs

--- Source/OculusQuestTemplate/OculusQuestTemplate.Build.cs
+++ Source/OculusQuestTemplate/OculusQuestTemplate.Build.cs
@@ -1,23 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class OculusQuestTemplate : ModuleRules
 {
 	public OculusQuestTemplate(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		// Standard Engine Dependencies
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		// VR Dependencies
+		PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay" });
 	}
 }
